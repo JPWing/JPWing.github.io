f1d3ff3487bca88a10ab7ecafd8d948ddeaa119f
JPWing <34347716+JPWing@users.noreply.github.com>
2017-12-14 12:16:31 +0000

    Navbar Sticky

--- js/script.js
+++ js/script.js
@@ -17,3 +17,13 @@ window.onclick = function(event) {
     }
 }
 
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("header");
+var sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
