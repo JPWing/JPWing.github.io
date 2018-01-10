d5c7271531c060f76fbfb3cba4dfe499d08cfbc6
JPWing <34347716+JPWing@users.noreply.github.com>
2018-01-10 13:55:11 +0000

    Renaming things

--- js/script.js
+++ js/script.js
@@ -26,23 +26,23 @@ $(function(){
 
     $(".theme1").click(function(){
         settheme(1);
-        localStorage.setItem("themestore", "1");
+        localStorage.setItem("storenumbertheme", "1");
         currenttheme = 1;
     });
 
     $(".theme2").click(function(){
         settheme(2);
-        localStorage.setItem("themestore", "2");
+        localStorage.setItem("storenumbertheme", "2");
         currenttheme = 2;
     });
     $(".theme3").click(function(){
         settheme(3);
-        localStorage.setItem("themestore", "3");
+        localStorage.setItem("storenumbertheme", "3");
         currenttheme = 3;
     });
     $(".theme0").click(function(){
         settheme(4);
-        localStorage.setItem("themestore", "4");
+        localStorage.setItem("storenumbertheme", "4");
         currenttheme = 4;
     });
 })
@@ -58,8 +58,8 @@ function init(){
     img3.src = 'assets/Background3.jpg';
     img4.src = 'assets/Background.jpg';
     if (typeof(Storage) !== "undefined") {
-        currenttheme = parseInt(localStorage.getItem("themestore"));
-        if (localStorage.getItem("themestore") === null)
+        currenttheme = parseInt(localStorage.getItem("storenumbertheme"));
+        if (localStorage.getItem("storenumbertheme") === null)
         {
             settheme(1);
             currenttheme = 1;
